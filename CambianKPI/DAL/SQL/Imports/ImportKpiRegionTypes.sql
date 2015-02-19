f49538d8b2b157bc16132457109c6f3c7f1d1a71
SabirAhmed85 <sabir.ahmed.web@gmail.com>
2015-02-19 20:35:14 +0000

    Project with backbone test

--- CambianKPI/DAL/SQL/Imports/ImportKpiRegionTypes.sql
+++
@@ -1,5 +0,0 @@
-SET IDENTITY_INSERT [dbo].[KpiRegionTypes] ON
-INSERT [dbo].[KpiRegionTypes] ([KpiRegionTypeID], [KpiRegionTypeName]) VALUES (1, N'Division')
-INSERT [dbo].[KpiRegionTypes] ([KpiRegionTypeID], [KpiRegionTypeName]) VALUES (2, N'Region')
-INSERT [dbo].[KpiRegionTypes] ([KpiRegionTypeID], [KpiRegionTypeName]) VALUES (3, N'Sub Region')
-SET IDENTITY_INSERT [dbo].[KpiRegionTypes] OFF
--- CambianKPI/DAL/SQL/Imports/clean-sql.ps1
+++
@@ -1,5 +0,0 @@
-param([String]$file)
-$newFile = "temp_" + $file
-select-string $file -pattern "^GO|^print|^$" -NotMatch | Select -ExpandProperty Line | Out-File $newFile -Encoding UTF8
-remove-item $file
-rename-item $newFile $file
